f3a11601767206eacc099391838330a1b719878d
Ankarrr <twan0817@gmail.com>
2017-03-01 17:08:35 +0800

    Starting to use postgresql and creating new models
    - use postgresql
    - create new models
    - show the tables on admin page

--- earphans/settings.py
+++ earphans/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'firstapp.apps.FirstappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'earphans.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'earphans',
+        'USER': 'anderson0817',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
