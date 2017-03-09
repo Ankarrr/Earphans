ce6bb77de5b9ef809f4b5dbffbbf698a30d48f6a
Ankarrr <twan0817@gmail.com>
2017-03-09 09:58:16 +0800

    Add results function
    - add results.html
    - update results function in view.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -24,8 +25,8 @@ def detail(request, question_id):
     return render(request, 'firstapp/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'firstapp/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
