d67f0655fadd0a64afad06e2a901e289ddf55ab9
Ankarrr <twan0817@gmail.com>
2017-03-17 11:34:39 +0800

    Add tests for IndexView
    add 2 tests in tests.py:
    - test_index_view_with_no_questions()
    - test_index_view_with_a_past_question()

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
 
 import datetime
 
@@ -17,3 +18,33 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(days=30)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('firstapp:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No firstapp are available.")
+        self.assertQuerysetEqual(response.context['object_list'], [])
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the
+        index page.
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('firstapp:index'))
+        self.assertQuerysetEqual(
+            response.context['object_list'],
+            ['<Question: Past question.>']
+        )
